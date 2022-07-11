7d6af1c51abf6fede7d8735cd1fd19add5339d2d
ssm825 <ssm39825@gmail.com>
2022-07-11 14:55:15 +0900

    프론트 초기 셋팅

---
+++ src/Router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Courses from './pages/Courses/Courses';
+import Carts from './pages/Carts/Carts';
+import DashBoard from './pages/DashBoard/DashBoard';
+import Detail from './pages/Detail/Detail';
+import SignUp from './pages/SignUp/SignUp';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:id" element={<Detail />} />
+        <Route path="/carts" element={<Carts />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default Router;
