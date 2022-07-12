17cc42f026fe09865a4778918942004e63bc98b1
ygh0627 <ygh970627@gmail.com>
2022-07-12 13:32:47 +0900

    Test

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,5 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 0px 115px;
+  display: flex;
+`;
 function Courses() {
-  return <div>Courses</div>;
+  return <Wrapper>Courses</Wrapper>;
 }
 
 export default Courses;
