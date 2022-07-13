46bef6499ca67f37939a83d3576f2b3af439027d
sming0112 <sming0112@gmail.com>
2022-07-13 16:53:43 +0900

    Add: Header layout, CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass, faCartShopping, faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faMagnifyingGlass,faCartShopping, faBell, faUser);
+
 function Header() {
   return(
     <Wrapper>
@@ -17,21 +22,44 @@ function Header() {
       <BottomWrapper>
         <BottomLeftWrapper>
           <img alt='codlearn-logo' src='images/logo.png' />
-          <span>강의</span>
-          <span>로드맵</span>
-          <span>멘토링</span>
-          <span>커뮤니티</span>
-          <span>채용정보</span>
+          <div>
+            <span>강의</span>
+          </div>
+          <div>
+            <span>로드맵</span>
+          </div>
+          <div>
+            <span>멘토링</span>
+          </div>
+          <div>
+            <span>커뮤니티</span>
+          </div>
+          <div>
+            <span>채용정보</span>
+          </div>
         </BottomLeftWrapper>
         <BottomRightWrapper>
           <Search>
-            <input></input>
-            <img></img>
+            <input>
+            </input>
+            <div>
+              <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+            </div>
           </Search>
-          <span>지식공유참여</span>
-          <icon></icon>
-          <icon></icon>
-          <icon></icon>
+          <Share>
+            <span>지식공유참여</span>
+          </Share>
+          <IconWrapper>
+            <div>
+              <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+            </div>
+            <div>
+              <FontAwesomeIcon icon="fa-solid fa-bell" />
+            </div>
+            <div>
+              <FontAwesomeIcon icon="fa-solid fa-user" />
+            </div>
+          </IconWrapper>
         </BottomRightWrapper>
       </BottomWrapper>
     </Wrapper>
@@ -78,6 +106,7 @@ const Employment = styled.div`
 const BottomWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 0 32px;
   height: 64px;
 `;
 
@@ -87,11 +116,17 @@ const BottomLeftWrapper = styled.div`
 
   img {
     width: 110px;
+    height: 100%;
   }
-`;
 
-const Logo = styled.div`
-
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 12px;
+    width: 60px;
+    height: 52px;
+  }
 `;
 
 const BottomRightWrapper = styled.div`
@@ -100,7 +135,48 @@ const BottomRightWrapper = styled.div`
 `;
 
 const Search = styled.div`
+  position: relative;
+  
+  input {
+    width: 128px;
+    height: 24px;
+    padding: 5px 9px;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    background: #f6f6f6;
 
+    &:hover{
+      border: 1px solid #5f5f5f;
+    }  
+  }  
+
+  div {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+`;
+
+const Share = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0 8px 8px;
+  width: 84px;
+  height: 48px;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 10px;
+    width: 24px;
+    height: 48px;
+  }
 `;
 
 export default Header;
