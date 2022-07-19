444a48dccdf8966f52686e71901aca2eff6134b9
ssm825 <ssm39825@gmail.com>
2022-07-19 21:19:50 +0900

    Add : 메인 subSlider Ui 구현

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainSlider from './Slider/MainSlider';
 import SearchBar from './SearchBar/SearchBar';
+import SubSlider from './Slider/SubSlider';
 import styled from 'styled-components';
 
 function Home() {
@@ -8,12 +9,11 @@ function Home() {
     <Main>
       <MainSlider />
       <SearchBar />
+      <SubSlider />
     </Main>
   );
 }
 
-const Main = styled.div`
-  height: 100vh;
-`;
+const Main = styled.div``;
 
 export default Home;
