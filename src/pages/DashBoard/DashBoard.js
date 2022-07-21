6c5b5f31b6723e21d186dfe8167382b57956956a
ssm825 <ssm39825@gmail.com>
2022-07-22 07:46:27 +0900

    Add : dashboard에 data 연결 중

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,72 +1,94 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { LoginContext } from '../../App';
 import MyPage from '../MyPage/MyPage';
 import styled from 'styled-components';
+import axios from 'axios';
 
 function DashBoard() {
-  //login 판별
   const [isLogin, setIsLogin] = useContext(LoginContext);
+  const [userData, setUserData] = useState([]);
+  console.log(isLogin);
+
+  const token = localStorage.getItem('token');
+
+  const boardApi = async () => {
+    const response = await axios.get(`http://localhost:10010/dashboard`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    setUserData(response.data);
+  };
+
+  useEffect(() => {
+    boardApi();
+  }, []);
+  console.log('data : ', userData.data);
   return (
-    <MyPage>
-      <BoardWrap>
-        <Column className="profile">
-          <ColumnFrame>
-            <ColumnTitle>
-              <span>😄 </span>
-              <span>(회원아이디)님 프로필</span>
-            </ColumnTitle>
-            <Content>
-              <div className="text">
-                <div className="logoIcon">
-                  <img src="images/icon.png" alt="icon" />
-                </div>
-                <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
-              </div>
-            </Content>
-          </ColumnFrame>
-        </Column>
-        <Column className="class">
-          <ColumnFrame>
-            <ColumnTitle>
-              <span>📖 </span>
-              <span>최근 학습 강의</span>
-            </ColumnTitle>
-            <Content>
-              <div className="text">
-                <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
-              </div>
-            </Content>
-          </ColumnFrame>
-        </Column>
-        <Column className="cart">
-          <ColumnFrame>
-            <ColumnTitle>
-              <span>🛒 </span>
-              <span>수강 바구니</span>
-            </ColumnTitle>
-            <Content>
-              <div className="text">
-                <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
-              </div>
-            </Content>
-          </ColumnFrame>
-        </Column>
-        <Column className="like">
-          <ColumnFrame>
-            <ColumnTitle>
-              <span>💖 </span>
-              <span>관심 있는 강의</span>
-            </ColumnTitle>
-            <Content>
-              <div className="text">
-                <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
-              </div>
-            </Content>
-          </ColumnFrame>
-        </Column>
-      </BoardWrap>
-    </MyPage>
+    <>
+      {isLogin ? (
+        <MyPage>
+          <BoardWrap>
+            <Column className="profile">
+              <ColumnFrame>
+                <ColumnTitle>
+                  <span>😄 </span>
+                  <span>{userData.data.user.name}님 프로필</span>
+                </ColumnTitle>
+                <Content>
+                  <div className="text">
+                    <div className="logoIcon">
+                      <img src="images/icon.png" alt="icon" />
+                    </div>
+                    <p> {userData.data.user.name}님, 남은 하루도 화이팅! 👋🏻</p>
+                  </div>
+                </Content>
+              </ColumnFrame>
+            </Column>
+            <Column className="class">
+              <ColumnFrame>
+                <ColumnTitle>
+                  <span>📖 </span>
+                  <span>최근 학습 강의</span>
+                </ColumnTitle>
+                <Content>
+                  <div className="text">
+                    <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
+                  </div>
+                </Content>
+              </ColumnFrame>
+            </Column>
+            <Column className="cart">
+              <ColumnFrame>
+                <ColumnTitle>
+                  <span>🛒 </span>
+                  <span>수강 바구니</span>
+                </ColumnTitle>
+                <Content>
+                  <div className="text">
+                    <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
+                  </div>
+                </Content>
+              </ColumnFrame>
+            </Column>
+            <Column className="like">
+              <ColumnFrame>
+                <ColumnTitle>
+                  <span>💖 </span>
+                  <span>관심 있는 강의</span>
+                </ColumnTitle>
+                <Content>
+                  <div className="text">
+                    <p> (회원아이디)님, 남은 하루도 화이팅! 👋🏻</p>
+                  </div>
+                </Content>
+              </ColumnFrame>
+            </Column>
+          </BoardWrap>
+        </MyPage>
+      ) : null}
+    </>
   );
 }
 
